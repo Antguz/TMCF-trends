7ba761cb377ad1d68716a16994bd699437df25db
J. Antonio Guzmán Q <antguz06@gmail.com>
2022-04-21 21:27:04 -0500

    Minor details for reproducibility

--- R/bayes.t.test.R
+++ R/bayes.t.test.R
@@ -8,84 +8,118 @@ library(BayesianFirstAid)
 library(MASS)
 library(raster)
 
+################################################################################
+### step 1 - Global mean estimated of trends
+################################################################################
+
+# This step is focused on compute the mean estimate of the climatic datasets on
+# the TMCF, and then test the probability if that estimate is higher or lower than 
+# zero.
+
 ### Load frame------------------------------------------------------------------
 frame <- fread("data/TMCF_slope.csv")
 frame <- subset(frame, remove != "yes") #Remove duplicates
-frame <- frame[, c(9:12, 15:16, 18, 20:22)]
+frame <- frame[, c(9:18)] #Select ECVs
+frame <- frame*10000 #Scale to x10-4
 
 ################################################################################
 ### Bayes application
 bayes_apply <- function(frame) {
   
+  # Create a copy to fill it
   frame <- as.data.frame(frame)
   col_names <- colnames(frame)
-  
   complete <- data.table()
   
+  # Loop over ECVs
   for(i in 1:length(col_names)) {
     
-    variable <- na.exclude(frame[, col_names[i]])*10000
+    # ECV name
+    variable <- na.exclude(frame[, col_names[i]])
     
+    # Apply bayes
     bayes <- bayes.t.test(variable)
     
+    # Get results
     results <- bayes$stats
     parameter <- row.names(results)
     variable <- rep(col_names[i], 5)
     results <- cbind(variable, parameter, as.data.table(results))
     
+    # Merge results
     complete <- rbind(complete, results)
     
   }
   
+  # Export complete table
   return(complete)
-  
 }
 
-#Run
+# Apply function
 bayes_results <- bayes_apply(frame)
+
+# Export results
 fwrite(bayes_results, "data/bayes_results.csv")
+
+################################################################################
+### Step 2 - Mean estimated of trends per realm
+################################################################################
+
+# This step is similar to the previous step except that it is evaluated at the 
+# realm level.
 
 ################################################################################
 ### Bayes t.test per realms
 frame <- fread("data/TMCF_slope.csv")
 frame <- subset(frame, remove != "yes") #Remove duplicates
-frame <- frame[, c(7, 9:12, 15:16, 18, 20:22)]
+frame <- frame[, c(7, 9:18)]
+frame[, c(2:11)] <- frame[, c(2:11)]*10000 #Scale
 
 #Function
 bayes_apply_realm <- function(frame) {
   
+  # Make a copy to fill
   frame <- as.data.frame(frame)
   col_names <- colnames(frame)[-1]
   col_realms <- unique(frame$realm)[1:4] #remove Oceania
-  
   complete <- data.table()
   
+  # Loop over realms
   for(i in 1:length(col_realms)) {
     
+    # Subset data
     sub_realm <- subset(frame, realm == col_realms[i])
     
+    # Loop over ECV
     for(ii in 1:length(col_names)) {
       
-      variable <- na.exclude(sub_realm[, col_names[ii]])*10000
+      # Remove missing values
+      variable <- na.exclude(sub_realm[, col_names[ii]])
       
+      # Apply bayes
       bayes <- bayes.t.test(variable)
       
+      # Get results
       results <- bayes$stats
       realm <- col_realms[i]
       parameter <- row.names(results)
       variable <- rep(col_names[ii], 5)
+      
+      # Compile results
       results <- cbind(realm, variable, parameter, as.data.table(results))
       
+      # Merge results
       complete <- rbind(complete, results)
       
     }
   }
+  
+  #Export filled table
   return(complete)
 }
 
-#Run
+# Apply function
 bayes_realm_results <- bayes_apply_realm(frame)
-fwrite(bayes_realm_results, "data/bayes_realm_results.csv")
 
-#
-1.448567
+# Export results
+fwrite(bayes_realm_results, "data/bayes_realm_results.csv")
--- R/plsr_model.R
+++ R/plsr_model.R
@@ -2,29 +2,44 @@
 ### Function to predict the importance and prediction of low cloud fraction
 ################################################################################
 
-###Select libraries-------------------------------------------------------------
+# The aim of these codes is to evaluate if trends of ECVs are able to predict 
+# trends of low-cloud fraction, and then see which ECV is the most important
+# for doing so. 
+
+### Select libraries------------------------------------------------------------
 library(data.table)
 library(pls)
 library(plsVarSel)
 
-###Load frame-------------------------------------------------------------------
+### Load frame------------------------------------------------------------------
 frame <- fread("data/TMCF_slope.csv")
 frame <- subset(frame, remove != "yes") #Remove duplicates
-frame <- frame[, c(9:12, 15, 16, 18, 20, 21, 22)]
+frame <- frame[, c(9:18)]
 frame <- na.exclude(frame)
 
-###Create model-----------------------------------------------------------------
-#Cross validation
+################################################################################
+### step 1 - Estimate the optimal number of components
+################################################################################
+
+# As part of the PLSR requirements, this function is focused on estimating the 
+# optimal number of components to predict low-cloud fraction.
+
+### Create model----------------------------------------------------------------
+# Cross validation
 model_comp <- function(frame, iterations = 1000) {
   
+  # Empty frame
   complete_frame <- data.table()
   
+  # Progress
   pb <- txtProgressBar(min = 1, max = iterations, style = 3)
   
+  # Number of iterations
   for(i in 1:iterations) {
     
     setTxtProgressBar(pb, i)
     
+    # Set model with cross validation
     model <- plsr(cloud ~ ., 
                   data = frame,
                   scale = TRUE,
@@ -35,60 +50,84 @@ model_comp <- function(frame, iterations = 1000) {
                   method = "oscorespls",
                   segments = 10)
     
+    # Export PRESS values
     df <- data.table(t(model$validation$PRESS))
     colnames(df) <- as.character(i)
     
+    # Fill frame
     complete_frame <- cbind(complete_frame, df)
     
   }
   
+  # Provide number of iteration
   component <- paste0("comp_", 1:9)
   
+  # Add components
   complete_frame <- cbind(component = component, complete_frame)
   
+  # Export
   return(complete_frame)
   
 }
 
+# Apply function
 results_components <- model_comp(frame, iterations = 1000) 
+
+# Export the optimal number of components
 fwrite(results_components, "data/PLSR_components.csv")
 
-#Performance function
+################################################################################
+### step 2 - Develop of a PLSR model using 9 components
+################################################################################
+
+# The following functions are focused on developing the final model knowing 
+# that nine components are required to reduce the RMSE of it. 
+
+# Performance function
 performance <- function(observed, predicted) {
   
+  # Make a copy
   obser <- observed
   pred <- predicted
   
+  # Get performance
   rss <- sum((obser - pred) ^ 2)  ## residual sum of squares
   tss <- sum((obser - mean(obser))^2)  ## total sum of squares
   
+  # Parameters of performance
   n <- length(predicted)
   Rsq <- 1 - (rss/tss)
   Bias <- mean(((obser - pred)/obser))
   RMSE <- sqrt(mean((obser - pred)^2))
   RMSE_P <- (RMSE/(max(obser)-min(obser)))*100
   
+  # Return values
   return(round(c(N = n, Rsq = Rsq, Bias = Bias, RMSE = RMSE, RMSE_P = RMSE_P), 6))
   
 }
 
-#Predicted model
+# Predicted model
 predicted_model <- function(frame, ncomp = 9, fraction = 0.5, iterations = 10000) {
   
+  # Empty frames to fill
   predicted_frame <- data.table()
   performance_frame <- data.table()
   VIP_frame <- data.table()
   coefficients_frame <- data.table()
   
+  # Progress
   pb <- txtProgressBar(min = 1, max = iterations, style = 3)
   
+  # Loop over iterations
   for(i in 1:iterations) {
     
     setTxtProgressBar(pb, i)
     
+    # Split data
     sub_sample <- frame[sample(nrow(frame), floor(nrow(frame)*fraction), 
                                replace = FALSE), ] 
     
+    # Develop model
     model <- plsr(cloud ~ ., 
                   data = sub_sample,
                   scale = TRUE,
@@ -98,39 +137,42 @@ predicted_model <- function(frame, ncomp = 9, fraction = 0.5, iterations = 10000
                   trace= FALSE, 
                   method = "oscorespls")
     
-    #Return predicted
+    # Return predicted
     value <- predict(model, newdata = frame, ncomp = ncomp, type= "response")[,,1]
     df <- as.data.table(matrix(value, ncol = 1))
     colnames(df) <- as.character(i)
     predicted_frame <- cbind(predicted_frame, df)
     
-    #Return performance
+    # Return performance
     perf <- as.data.table(matrix(c(i,performance(frame$cloud, value)), nrow = 1))
     performance_frame <- rbind(performance_frame, perf)
     
-    #Retrun VIP_frame
+    # Return VIP_frame
     vip <- as.data.table(matrix(c(i, VIP(model, opt.comp = ncomp)), nrow = 1))
     VIP_frame <- rbind(VIP_frame, vip)
     
-    #Coefficients
+    # Coefficients
     coeff <- as.data.table(matrix(c(i, coef(model, ncomp = ncomp, intercept=TRUE)), nrow = 1))
     coefficients_frame <- rbind(coefficients_frame, coeff)
     
   }
   
+  # Provide names to performance
   colnames(performance_frame) <- c("iteration", "N", "Rsq", 
                                    "Bias", "RMSE", "RMSE_P")
   
-  
+  # Provide names to VIP
   colnames(VIP_frame) <- c("iteration", "temperature", "min_temperature",
                            "max_temperature", "dewpoint", "pressure", 
                            "precipitation", "vswc", "ET", "PET")
   
+  # Provide names coefficients
   colnames(coefficients_frame) <- c("iteration", "intercept", "temperature", 
                                     "min_temperature", "max_temperature", 
                                     "dewpoint", "pressure", "precipitation", 
                                     "vswc", "ET", "PET")
   
+  # Return compiled results
   results <- list(predicted = predicted_frame,
                   performance = performance_frame,
                   VIP = VIP_frame,
@@ -140,8 +182,11 @@ predicted_model <- function(frame, ncomp = 9, fraction = 0.5, iterations = 10000
   
 }
 
+# Apply function
 results_model <- predicted_model(frame, ncomp = 9, fraction = 0.5, iterations = 5000)
-a <- results_model$predicted
-results_model$performance
-results_model$VIP
-results_model$Coefficients
+
+# Export results
+fwrite(results_model$predicted, "data/PLSR_predicted.csv")
+fwrite(results_model$performance, "data/PLSR_performance.csv")
+fwrite(results_model$VIP, "data/PLSR_VIP.csv")
+fwrite(results_model$Coefficients, "data/PLSR_coefficients.csv")
